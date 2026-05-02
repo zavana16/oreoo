9a8f54dc4594cdf60c22e400491cce68b1984fa7
alsen <zavaa228@gmail.com>
2026-05-02 12:49:53 +0700

    menambah from login

---
+++ zv.js
@@ -0,0 +1,15 @@
+function proseslogin() {
+    let inputUser = document.getElementById("username").value;
+    let inputPass = document.getElementById("password").value;
+
+    let adminUser = "admin" ;
+    let adminPass = "admin";
+    
+    if (inputUser === adminUser && inputPass === adminPass) {
+        alert("login berhasil! mengalihkan ke dashboard....") ;
+        window.location.href = "dashboard.html";
+    
+    } else {
+        alert("username tau pasword salah. silahkn coba lagi");
+    }
+}
